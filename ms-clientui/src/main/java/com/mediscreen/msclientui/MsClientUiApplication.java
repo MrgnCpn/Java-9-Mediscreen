6009ffc62e399f8bf3cb0f3ed765397e003edd1e
MrgnCpn <morgancpn.dev@gmail.com>
2020-12-28 17:17:55 +0100

    Create and configure ms-clientui

---
+++ ms-clientui/src/main/java/com/mediscreen/msclientui/MsClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.msclientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientUiApplication.class, args);
+	}
+
+}
